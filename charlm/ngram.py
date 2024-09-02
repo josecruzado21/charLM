6cdbffad67e9717968d2d62f4e327e4fccdfa338
Jose Cruzado <josecruzado2103@gmail.com>
2024-09-02 16:17:26 -0500

    feat: move from jupyter to ngrams

---
+++ charlm/ngram.py
@@ -0,0 +1,73 @@
+class ngram:
+    def __init__(self, size, smoothing_factor=0):
+        self.size = size
+        self.smoothing_factor = smoothing_factor
+    
+    def __count_ngrams(self, ngrams_list):
+        ngrams_counts = {}
+        for ngram in ngrams_list:
+            if ngram in ngrams_counts:
+                ngrams_counts[ngram] += 1
+            else:
+                ngrams_counts[ngram] = 1
+        return ngrams_counts
+
+    def __ngram_list_updated(self, list_object, n, include_intermediate_ns = True):
+        ngrams = []
+        iterator = range(2, n+1) if include_intermediate_ns else [n]
+        for n_of_grams in iterator:
+            for element in list_object:
+                element = ["<>"] + list(element) + ["<>"]
+                if len(element)>=(n_of_grams-2):
+                    for idx in range(len(element) - n_of_grams + 1):
+                        ngrams.append(("".join(element[idx:idx+n_of_grams-1]), "".join(element[idx+n_of_grams-1])))
+        self.ngrams = ngrams
+        self.ngrams_frequencies = self.__count_ngrams(ngrams)
+
+    def __calculate_probabilities(self, ngrams_count, smoothing_factor = 0):
+        previous_chars = sorted(list(set([i[0] for i in ngrams_count.keys()])))
+        next_char = sorted(list(set([i[1] for i in ngrams_count.keys()])))
+        probabilities = np.zeros((len(previous_chars), len(next_char)))
+        for idx_f, f in enumerate(previous_chars):
+            for idx_n, n in enumerate(next_char):
+                probabilities[(idx_f, idx_n)] = counts.get((f, n), smoothing_factor)
+        probabilities = probabilities / probabilities.sum(axis=1, keepdims=True)
+        self.previous_chars = previous_chars
+        self.next_char = next_char
+        self.estimated_probabilitites = probabilities
+
+    def __generate_word(self, previous_chars, next_char, probabilities, n):
+        first_char = str(np.random.choice(nexts, size=1, replace=True, p=probabilities[firsts.index("<>")])[0])
+        word = first_char
+        while True:
+            prev_ngram = word[-(n-1):] if len(word)>=(n-1) else '<>'+word
+            next_char = str(np.random.choice(nexts, size=1, replace=True, p=probabilities[firsts.index(prev_ngram)])[0])
+            if next_char == "<>":
+                break
+            word += next_char
+        return word
+
+    def __generate_words(self, number_of_words, previous_chars, next_char, probabilities, n):
+        words = []
+        for i in range(number_of_words):
+            words.append(self.__generate_word(previous_chars, next_char, probabilities, n))
+        return words
+
+    def __calculate_perplexity_of_word(word, previous_chars, next_char, probabilities, n):
+        word = ["<>"] + list(word) + ["<>"]
+        predictor_grams = []
+        for idx_char, char in enumerate(word[:-1]):
+            predictor_grams.append("".join(word[max(0, idx_char - (n-1) + 1):idx_char+1]))
+        perplexity = 1
+        for predictor, test in zip(predictor_grams, word[1:]):
+            try:
+                probability = float(probabilities[previous_chars.index(predictor)][next_char.index(test)])
+                probability = probability if probability>0 else 0.001
+            except:
+                probability = 1
+            perplexity *= (probability)**(-1/len(predictor_grams))
+        return perplexity
+
+    def fit(self, X):
+        self.__ngram_list_updated(X, self.size)
+        self.__calculate_probabilities(self.ngrams_count, self.smoothing_factor)

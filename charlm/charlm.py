b1cf80e4b1c75c17358d08d9d3d17116a54b80fe
Jose Cruzado <59905385+josecruzado21@users.noreply.github.com>
2025-01-23 18:13:39 -0600

    feat: add batch normalization (#7)
    feat: add batch normalization

--- charlm/charlm.py
+++ charlm/charlm.py
@@ -110,18 +110,20 @@ class CharLM:
             if initialization == "random":
                 normalization_factor = 1
             elif initialization == "he":
-                normalization_factor = (2/fan_in)**0.5 # The factor 2/fan_in is used to scale the weights to match the variance of the input data
+                zero_out_biases = True
+                gain = 2**0.5
+                normalization_factor = ((1/fan_in)**0.5)*gain # The factor 2/fan_in is used to scale the weights to match the variance of the input data
                 # This is a common heuristic for He initialization (follows the original paper)
             
             # Initialize the weights and biases
-            if weights_biases_dbn == "normal":  
+            if weights_biases_dbn == "normal":
                 initialized_weights = torch.randn(
-                    (fan_in, fan_out), dtype=torch.float64)*normalization_factor*(1-zero_out_weights)
-                initialized_biases = torch.randn(fan_out, dtype=torch.float64)*(1-zero_out_weights)
+                    (fan_in, fan_out), dtype=torch.float32)*normalization_factor*(1-zero_out_weights)
+                initialized_biases = torch.randn(fan_out, dtype=torch.float32)*(1-zero_out_biases)
             elif weights_biases_dbn == "uniform":
                 initialized_weights = torch.rand(
-                    (fan_in, fan_out), dtype=torch.float64)*normalization_factor*(1-zero_out_weights)
-                initialized_biases = torch.rand(fan_out, dtype=torch.float64)*(1-zero_out_biases)
+                    (fan_in, fan_out), dtype=torch.float32)*normalization_factor*(1-zero_out_weights)
+                initialized_biases = torch.rand(fan_out, dtype=torch.float32)*(1-zero_out_biases)
             initialized_weights.requires_grad_()
             initialized_biases.requires_grad_()
             weights.append(initialized_weights)
@@ -132,7 +134,8 @@ class CharLM:
             X_train, 
             y_train, 
             neurons_per_layer,
-            activations, 
+            activations,
+            normalize_layer,
             size_of_embeddings,
             epochs, 
             learning_rate,
@@ -140,7 +143,8 @@ class CharLM:
             weights_biases_dbn="normal",
             zero_out_weights=False,
             zero_out_biases=False,
-            batch_size=1):
+            batch_size=1,
+            normalize_pre_activation=True):
         """
         Trains the character-level language model using a multilayer perceptron (MLP) architecture.
 
@@ -150,6 +154,7 @@ class CharLM:
             neurons_per_layer (list of int): List specifying the number of neurons in each hidden layer of the MLP.
             activations (list of str): List of activation functions (as string names) to use for each layer.
                                      The last activation must be "softmax".
+            normalize_layer (list of bool): List indicating whether to apply normalization for each layer.
             size_of_embeddings (int): Dimensionality of the character embeddings.
             epochs (int): Number of training epochs.
             learning_rate (float): Learning rate for gradient-based optimization.
@@ -167,16 +172,21 @@ class CharLM:
                 - If [0, 1]: Treated as a fraction of the training set size
                 - If > 1: Treated as the actual batch size in number of samples
                 Defaults to 1 (full batch).
+            normalize_pre_activation (bool, optional): If True, applies normalization before activation functions.
+                If False, applies normalization after activation functions. Defaults to True.
 
         Returns:
             None: The method updates the model's parameters in place
         """
+        if len(normalize_layer) != len(activations):
+            raise ValueError("The number of normalization layers must match the number of activations")
+        # Check that the last activation is softmax 
         if activations[-1]!="softmax":
             raise ValueError("Only softmax activation function is supported for the output layer")
         self.activations = activations
         # Initializing character embeddings as trainable parameters
         embeddings = torch.randn((len(self.idx_to_char), 
-                                 size_of_embeddings), requires_grad = True, dtype=torch.float64)
+                                 size_of_embeddings), requires_grad = True, dtype=torch.float32)
         self.initial_embeddings = embeddings.clone().detach()
         # Initialize the weights and biases for each layer in the MLP
         weights = []
@@ -189,15 +199,25 @@ class CharLM:
                                                              weights_biases_dbn,
                                                              zero_out_weights,
                                                              zero_out_biases)
+        
+        # Initialize the normalization parameters
+        normalization_parameters = [torch.tensor([], requires_grad=True)]*len(weights) # This list will store the gamma and beta parameters for each layer
+        running_means_stds = [torch.tensor([])]*len(weights)
+        for idx, layer in enumerate(normalize_layer):
+            if layer:
+                normalization_parameters[idx] = torch.cat((torch.ones((1, neurons_per_layer[idx+1]), dtype=torch.float32),
+                                                          torch.zeros((1, neurons_per_layer[idx+1]), dtype=torch.float32)),
+                                                          dim=0).requires_grad_()
+                running_means_stds[idx] = torch.zeros((2, neurons_per_layer[idx+1]))
+
         self.initial_weights = [element.clone().detach() for element in weights]
         self.initial_biases = [element.clone().detach() for element in biases]
-        parameters = [embeddings] + weights + biases
+        parameters = [embeddings] + weights + biases + normalization_parameters
         batch_losses = [] # Track loss over batches for analysis
         if 0 < batch_size <= 1:
             batch_size = int(X_train.shape[0]*batch_size) 
         elif batch_size > 1:
             batch_size = min(int(batch_size), X_train.shape[0])
-
         for _ in tqdm(range(epochs)): # Loop over the number of epochs
             # Gradient descent step: Update weights, biases, and embeddings
             # The update is done at the beginning of the epoch so the weights and biases saved are the ones
@@ -210,19 +230,44 @@ class CharLM:
 
             # Randomly sample a batch of the training set
             ix = torch.randperm(X_train.shape[0])[:batch_size]
-            
             # Forward pass: Embedding lookup and feedforward through the MLP
             X = embeddings[X_train[ix]] # Get the embeddings for the input character sequences
             input_to_next_layer = X.view((X.shape[0], X.shape[1]*X.shape[2])) # Flatten input for the first layer
             
             # Feedforward through all layers
-            for weight, bias, activation in zip(weights, biases, activations):
+            for n_layer, (weight, bias, activation, normalization_parameter) in enumerate(zip(
+                                                                    weights, 
+                                                                    biases, 
+                                                                    activations, 
+                                                                    normalization_parameters)):
                 A = input_to_next_layer @ weight + bias
+                # Normalize before the activation function if indicated by normalize_pre_activation
+                # Done for all layers
+                if (normalize_layer[n_layer]) & (normalize_pre_activation):
+                    gamma = normalization_parameter[0]
+                    beta = normalization_parameter[1]
+                    mean_batch = A.mean(dim=0, keepdim=True)
+                    std_batch = A.std(dim=0, keepdim=True)
+                    running_means_stds[n_layer][0] = running_means_stds[n_layer][0]*(1-0.001) + mean_batch*0.001
+                    running_means_stds[n_layer][1] = running_means_stds[n_layer][1]*(1-0.001) + std_batch*0.001
+                    A = ((A - mean_batch)/(std_batch + 1e-5))*gamma + beta  
+
                 activation_function = getattr(A, activation)
                 if activation == "softmax":
                     A_activation = activation_function(dim=1)
                 else:
                     A_activation = activation_function()
+                    # Normalize after the activation function if indicated by normalize_pre_activation
+                    # Only done for layers different from the output layer
+                    if (normalize_layer[n_layer]) & (not normalize_pre_activation):
+                        gamma = normalization_parameter[0]
+                        beta = normalization_parameter[1]
+                        mean_batch = A_activation.mean(dim=0, keepdim=True)
+                        std_batch = A_activation.std(dim=0, keepdim=True)
+                        running_means_stds[n_layer][0] = running_means_stds[n_layer][0]*(1-0.001) + mean_batch*0.001
+                        running_means_stds[n_layer][1] = running_means_stds[n_layer][1]*(1-0.001) + std_batch*0.001
+                        A_activation = ((A_activation - mean_batch)/(std_batch + 1e-5))*gamma + beta
+
                 input_to_next_layer = A_activation
             
             logits = A # logits are the last linear combination (before the application of the softmax activation)
@@ -236,6 +281,10 @@ class CharLM:
         self.final_embeddings = embeddings
         self.final_weights = weights
         self.final_biases = biases
+        self.final_normalization_parameters = normalization_parameters
+        self.running_means_stds = running_means_stds
+        self.normalize_layer = normalize_layer
+        self.normalize_pre_activation = normalize_pre_activation
 
     def calculate_loss(self, X, y):
         """
@@ -251,15 +300,26 @@ class CharLM:
         with torch.no_grad():
             X = self.final_embeddings[X]
             inputs_to_next_layer = X.view((X.shape[0], X.shape[1]*X.shape[2]))
-            for weight, bias, activation in zip(self.final_weights, 
-                                                self.final_biases, 
-                                                self.activations):
+            for weight, bias, activation, running_means_stds, normalize_layer, normalization_parameter in zip(self.final_weights, 
+                                                                    self.final_biases, 
+                                                                    self.activations,
+                                                                    self.running_means_stds,
+                                                                    self.normalize_layer,
+                                                                    self.final_normalization_parameters):
                 A = inputs_to_next_layer @ weight + bias
+                if self.normalize_pre_activation & normalize_layer:
+                    gamma = normalization_parameter[0]
+                    beta = normalization_parameter[1]
+                    A = ((A - running_means_stds[0])/running_means_stds[1])*gamma + beta
                 activation_function = getattr(A, activation)
                 if activation == "softmax":
                     A_activation = activation_function(dim=1)
                 else:
                     A_activation = activation_function()
+                    if (not self.normalize_pre_activation) & normalize_layer:
+                        gamma = normalization_parameter[0]
+                        beta = normalization_parameter[1]
+                        A_activation = ((A_activation - running_means_stds[0])/running_means_stds[1])*gamma + beta
                 inputs_to_next_layer = A_activation
             logits = A
             loss = F.cross_entropy(logits, y)
@@ -279,15 +339,26 @@ class CharLM:
         with torch.no_grad():
             X_pred = self.final_embeddings[X]
             inputs_to_layers = [X_pred.view((X_pred.shape[0], X_pred.shape[1]*X_pred.shape[2]))]
-            for weight, bias, activation in zip(self.final_weights, 
+            for weight, bias, activation, running_means_stds, normalize_layer, normalization_parameter in zip(self.final_weights, 
                                                 self.final_biases, 
-                                                self.activations):
+                                                self.activations,
+                                                self.running_means_stds,
+                                                self.normalize_layer,
+                                                self.final_normalization_parameters):
                 A = inputs_to_layers[-1] @ weight + bias
+                if self.normalize_pre_activation & normalize_layer:
+                    gamma = normalization_parameter[0]
+                    beta = normalization_parameter[1]
+                    A = ((A - running_means_stds[0])/running_means_stds[1])*gamma + beta
                 activation_function = getattr(A, activation)
                 if activation == "softmax":
                     A_activation = activation_function(dim=1)
                 else:
                     A_activation = activation_function()
+                    if (not self.normalize_pre_activation) & normalize_layer:
+                        gamma = normalization_parameter[0]
+                        beta = normalization_parameter[1]
+                        A_activation = ((A_activation - running_means_stds[0])/running_means_stds[1])*gamma + beta
                 inputs_to_layers.append(A_activation)
         return A_activation
 
--- demo/mlp_demo.py
+++ demo/mlp_demo.py
@@ -1,4 +1,9 @@
 import random
+# from charlm.charlm import CharLM
+
+import os
+import sys
+sys.path[0] = os.path.abspath(os.path.join(os.getcwd()))
 from charlm.charlm import CharLM
 
 file = "names"
@@ -30,10 +35,12 @@ model.fit(X_train,
           y_train, 
           neurons_per_layer = [100, len(model.char_to_idx.keys())], 
           activations = ["tanh", "softmax"], 
+          normalize_layer = [True, True],
+          normalize_pre_activation = True,
           size_of_embeddings = 2, 
-          epochs=10000,
+          epochs=50000,
           learning_rate=0.1,
-          initialization="he",
+          initialization="random",
           weights_biases_dbn="normal",
           zero_out_weights=False,
           zero_out_biases=False,
@@ -44,7 +51,7 @@ print(f"\t\tLoss (dev) n-gram LM: {model.calculate_loss(X_dev, y_dev):.5f}")
 print(f"\t\tLoss (test) n-gram LM: {model.calculate_loss(X_test, y_test):.5f}")
 
 print("\tPrediction:")
-words = model.generate_words(1000)
+words = model.generate_words(100)
 with open(f"./data/output/generated_{file}_mlp.txt", "w") as f:
     f.writelines(f"{word}\n" for word in words)
 
